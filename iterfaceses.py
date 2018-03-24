425c6e0165d719e758b78cd3e0b5f6d9a2b21ffd
Jarosław Wieczorek <jaroslaw.wieczorek@sealcode.org>
2018-03-24 14:55:16 +0100

    add interfaces GenericBlockHeader

--- iterfaceses.py
+++ iterfaceses.py
@@ -12,7 +12,7 @@ from interface import implements, Interface
 class UI(Interface):
     
     def __init__(self, address):
-        pass 
+        pass
     def method(self):
         pass
 
@@ -21,27 +21,66 @@ class Wallet(implements(UI)):
     def __init__(self, address):
         super()
         pass
-    def method(self, f):
+    
+    def method(self):
         print("to działa")
     
     def method_b(self):
         print("to działa")
+        
 
-
+class GenericBlockHeader(Interface):
+    def __init__(self, index, previousHash, timestamp, difficulty, nonce):
+        self.index = index
+        self.previousHash = previousHash
+        self.timestamp = timestamp
+        self.difficulty = difficulty
+        self.nonce = nonce
+        
+class BlockHeader(implements(GenericBlockHeader)):
+    def __init__(self, index, previousHash, timestamp, difficulty, nonce):
+        self.index = index
+        self.previousHash = previousHash
+        self.timestamp = timestamp
+        self.difficulty = difficulty
+        self.nonce = nonce
+        
 
 class GenericBlock(Interface):
+    
+    def __init__(self, BlockHeader):
+          # number, currentHash, previousHash, timestamp, Transaction, difficulty, number
+          pass
+        
     def method(self, a, b):
         pass
+    
 
 class Block(implements(GenericBlock)):
-    def method(self, a,b):
+    def __init__(self, BlockHeader):
+        pass
+    
+    def method(self, a, b):
         return "This should work"
 
 
-b = Block()
+
+
+class Transaction():
+    pass
+
+class TransIN():
+    pass
+
+class TransOUT():
+    pass
+
+    
+h = BlockHeader(0, "dfsfisd", 1318320, 1, 0)
+
+b = Block(h)
 
 print(b.method(4,4))
 
 w = Wallet(48348)
-
-w.method_b()
+Wallet(23)
