e9c90e8efe19456dbfb8a0e9bdc53a7a1d9d2469
chris642 <krzysztof.orczyk@student.put.poznan.pl>
2018-05-06 14:19:25 +0200

    Added docstrings to existing TransactionPool methods.

--- src/transactionPool.py
+++ src/transactionPool.py
@@ -5,9 +5,11 @@ class TransactionPool:
     transactionPool = []
 
     def getTransactionPool(self):
+        """Returns the local pool of Transactions."""
         return deepcopy(self.transactionPool)
 
     def addToTransactionPool(self, transaction, unspentTxOuts):
+        """Adds transactions to the Pool, validates before hand."""
         if not validateTransaction(transaction, unspentTxOuts):
             raise ValueError("Invalid tx, can not add.")
         if not isValidTxForPool(transaction, self.transactionPool):
@@ -17,6 +19,8 @@ class TransactionPool:
         self.transactionPool.append(transaction)
 
     def hasTxIn(self, txIn, unspentTxOuts):
+        """Returns True if txIn Id and Index exist in the unspentTxOuts."""
         foundTxIn = [x for x in unspentTxOuts if x.txOutId == txIn.txOutId and x.txOutIndex == txIn.txOutIndex][0]
         if foundTxIn:   #TO_DO Verify if this works properly.
             return True
+
