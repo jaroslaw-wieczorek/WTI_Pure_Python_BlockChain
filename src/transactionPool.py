bb6835c505e1b0f87033a895f6c92c4bd6963b31
chris642 <krzysztof.orczyk@student.put.poznan.pl>
2018-05-06 14:38:04 +0200

    Added updateTransactionPool to the transactionPool class.

--- src/transactionPool.py
+++ src/transactionPool.py
@@ -24,3 +24,24 @@ class TransactionPool:
         if foundTxIn:   #TO_DO Verify if this works properly.
             return True
 
+    def updateTransactionPool(self, unspentTxOut):
+        """Updates the pool by removing invalid transactions.
+        Invalid transactions can be:
+        *Transactions already mined,
+        *The unspent out was spent by some other transaction, making this one invalid."""
+
+        invalidTxs = []
+        for tx in self.transactionPool:
+            for txIn in tx.transIN:
+                if not self.hasTxIn(txIn, unspentTxOut):
+                    invalidTxs.append(tx)
+                    break
+        if len(invalidTxs) > 0:
+            print("Removing transactions from the Pool:")
+            print(invalidTxs)
+            print("They where probably mined or spent elsewhere.")
+            self.transactionPool = list(set(self.transactionPool)-set(invalidTxs))
+            #We should not have duplicate values, and this approach is suprisingly fast!
+            #252s vs 0.75s for 500000 array.
+            #Which is a good thing because theoretically we could have very big lists here.
+            #Source: https://stackoverflow.com/a/30353802
