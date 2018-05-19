9917f4d79fde8d8eecc59073f2c91950cfab6fd8
Jarosław Wieczorek <jaroslaw.wieczorek@sealcode.org>
2018-05-19 14:07:38 +0200

    add generate and save private key, add export public key from private

--- app/src/wallet.py
+++ app/src/wallet.py
@@ -19,6 +19,7 @@ from Crypto.Signature import PKCS1_v1_5
 from Crypto.Hash import SHA256 
 from base64 import b64encode, b64decode 
 
+from Crypto import Random
 
 # importing data accc
 lib_path = os.path.abspath(os.path.join(__file__, '..','..','rsa_keys'))
@@ -30,21 +31,37 @@ class Wallet(implements(UI)):
     
     def __init__(self, address):
         # PRIVATE KEY
-        self.__key = open(lib_path+'/key', "r").read()
+        self.__privateFileKey = open(lib_path+'/key.pem', "r").read()
        
         # PUBLIC KEY
-        self.__publ_key = None
+        self.__publicKey = None
         #self.__pub_key = open("key.pub", "r").read()
         super()
         
     def getPrivateFromWallet(self) -> str: 
-        self.__priv_key = RSA.importKey(self.__key) 
-        return self.__priv_key
+        self.__privKey = RSA.importKey(self.__privateFileKey) 
+        return self.__privKey
     
     def getPublicFromWallet(self) -> str: 
-        self.__publ_key = self.__priv_key.publickey().exportKey()
-        return self.__publ_key
+        self.__publKey = self.getPrivateFromWallet().publickey().exportKey()
+        return self.__publKey
     
+    
+    def generatePrivateKey(self) -> str:
+        '''
+            Generate an RSA keypair in PEM format
+            param: bits The key length in bits
+            Return private key and public key
+        '''
+        rng = Random.new().read
+        new_key = RSA.generate(1024, rng) 
+        self.__privKey = new_key.exportKey("PEM") 
+        
+        with open(lib_path+'/key.pem', 'wb') as pem:
+            pem.write(self.__privKey)
+            pem.close()
+        
+        return str(self.__priv_key)
     
 
     def method(self):
