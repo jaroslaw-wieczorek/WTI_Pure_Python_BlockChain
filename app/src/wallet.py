fff60c333f987c7fece7c089e739baf8e2a58d44
Jarosław Wieczorek <jaroslaw.wieczorek@sealcode.org>
2018-05-20 18:10:46 +0200

    add filterTranPoolTrans to wallet

--- app/src/wallet.py
+++ app/src/wallet.py
@@ -7,10 +7,12 @@ from .generics.interfaces import UI
 import os
 import re
 import sys
+import json
 import hashlib
 import datetime
 import binascii
 import operator 
+import itertools
 from datetime import timezone
 from functools import reduce
 
@@ -22,6 +24,12 @@ from base64 import b64encode, b64decode
 from Cryptodome import Random
 from .unspentOutTrans import UnspentOutTrans
 from .transactionMethods import TransMethods
+from .transactionPool import TransactionPool
+from .transOUT import TransOUT
+from .transIN import TransIN
+from .transaction import Transaction
+
+
 # importing data accc
 lib_path = os.path.abspath(os.path.join(__file__, '..','..','rsa_keys/key.pem'))
 current_file_path = os.path.abspath(os.path.join(__file__))
@@ -45,6 +53,7 @@ class Wallet(implements(UI), TransMethods):
     def getPrivateFromWallet(self) -> str: 
         #self.__privateKey = RSA.importKey(self.__privateFileKey) 
         return self.__privateKey 
+    
     
     def getPublicFromWallet(self) -> str: 
         #self.__publKey = self.getPrivateFromWallet().publickey().exportKey()
@@ -88,7 +97,82 @@ class Wallet(implements(UI), TransMethods):
             balance += trans.amount
      
         return balance
+
+
+    def findUnspentOutTrans(self, ownerAddres : str, unspentOutsTrans : UnspentOutTrans):
+        return list(filter((lambda trans: trans.address == ownerAddres), unspentOutsTrans))
     
+    
+    def findOutsTransForAmount(self, amount, myUnspentOutsTrans: UnspentOutTrans):
+        currentAmount = 0
+        includeUnspentOutTrans = []
+        for myUnspentOutTrans in myUnspentOutsTrans:
+            includeUnspentOutTrans.insert(myUnspentOutTrans)
+            currentAmount = currentAmount + myUnspentOutTrans.amount
+            if currentAmount >= amount:
+                leftOverAmount = currentAmount - amount
+                return {includeUnspentOutTrans, leftOverAmount}
+        eMsg = 'Cannot create transaction from the available unspent transaction outputs.' + ' Required amount: ' + str(amount) 
+        + '. Available unspentOutsTrans: ' + json.dumps(myUnspentOutsTrans)
+        raise Exception(eMsg)
+        
+
+    def createOutsTrans(self, receiverAddress : str, myAddress : str, amount, leftOverAmount):
+        outTrans1 : TransOUT = TransOUT(receiverAddress, amount)
+        if leftOverAmount == 0:
+            return [outTrans1]
+        else:
+            leftOverAmount = TransOUT(myAddress, leftOverAmount)
+            return [outTrans1, leftOverAmount]
+    
+    
+    def first_true(iterable, default=False, pred=None):
+        return next(filter(pred, iterable), default)
 
 
+    def filterTranPoolTrans(unspentOutsTrans : UnspentOutTrans, 
+                            transactionPool : Transaction) -> UnspentOutTrans:
+        transINs : TransIN = list(itertools.flatten(map(
+                (lambda trans : trans.transINs) , transactionPool)))
 
+        removable : list = []
+        
+        for unspent in unspentOutsTrans:
+            transIN = (transINs, None, (
+                    lambda transIn: transIn.transOutId == unspent.transOutId
+                    and transIn.transOutIndex == unspent.transOutIndex
+                    ))
+            if transIN == None:
+                pass
+            else:
+                removable.append(unspent)
+        
+        filteredTransactions = [trans for trans in unspentOutsTrans 
+                                      if trans not in removable]
+       
+        return filteredTransactions
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
